de23479846d3e614bd31d27d66b7ce764c525b5b
luque <miguelangelluque21@gmail.com>
2020-10-30 14:19:52 +0100

    Commit inicial

---
+++ app/src/main/java/com/incidences/incidencesapp/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.incidences.incidencesapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
